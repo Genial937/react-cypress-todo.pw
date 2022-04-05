fb31feb8410cd6065271a20776b8c8b5da072f58
Genial <odenyothadeus@gmail.com>
2022-04-06 00:25:23 +0300

    Playwright automated configs

---
+++ playwright.config.ts
@@ -0,0 +1,16 @@
+import {PlaywrightTestConfig} from '@playwright/test'
+
+const config: PlaywrightTestConfig = {
+    use:{
+        headless: true,
+        channel: "chrome",
+        screenshot: "only-on-failure",
+        video: "retain-on-failure",
+
+    },
+    testMatch: ["**.spec.js"],
+    retries: 0,
+    reporter: [["dot"],["json", {outputFile: "test-result.json"}]]
+}
+
+export default config;
