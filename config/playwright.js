ce64d5088d963c0c08fe643db918872aa0eeda7c
Thadeus Odenyo <odenyothadeus@gmail.com>
2022-04-05 23:10:01 +0300

    Initial tests implimentation

--- config/playwright.js
+++ config/playwright.js
@@ -5,7 +5,7 @@ exports.playwright = async function playwright() {
     const browser = await webkit.launch({ headless: false });
     const context = await browser.newContext()
     const page = await context.newPage();
-    await page.goto('https://wso2.com/identity-and-access-management/',
+    await page.goto('/Applications/Safari.app',
         { waitUntil: 'load', timeout: 10000 }).then(() => {
             console.log('success')
         }).catch((res) => {
