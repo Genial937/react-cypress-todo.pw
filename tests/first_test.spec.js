c778a5a5420b6b601f5e55fd0049f8407c499c69
Genial <odenyothadeus@gmail.com>
2022-04-19 11:29:02 +0300

    Jobs

--- tests/first_test.spec.js
+++ tests/first_test.spec.js
@@ -1,5 +1,6 @@
 const { test, expect } = require('@playwright/test');
 
+// First test
 test('basic test', async ({ page }) => {
   await page.goto('http://localhost:3000/');
   const title = page.locator('.Heading');
